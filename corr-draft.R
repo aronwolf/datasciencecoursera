629e1a5d9bcdec09dbdace57e86a32676d122cd4
Aron Wolf <aron.wolf@proquest.com>
2016-07-15 10:49:19 -0700

    File update for week 2 / homework assignment 1

--- corr-draft.R
+++ corr-draft.R
@@ -1,25 +1,26 @@
 corr <- function(directory, threshold = 0) {
     
     pollutant_data <- data.frame()
-    correlation_ccoefficients <- data.frame()
+    correlation_ccoefficients <- numeric(0)
 
     complete_observations <- complete(directory)
     complete_observations_at_threshold <- complete_observations[which(complete_observations$nobs > threshold),]
     
-    k <- data.frame(table(complete_observations_at_threshold$ID))
+    k <- c(complete_observations_at_threshold$ID)
     
     files_full_path <- list.files(directory, full.names = TRUE)
     for (i in 332) {
         pollutant_data <- rbind(pollutant_data, read.csv(files_full_path[i]))
         }
       
-    for (j in k) {
-        observation_set <- read.csv(files_full_path[as.numeric(complete_observations_at_threshold[j,1])])
+    for (j in length(k)) {
+        file_to_iterate <- as.numeric(complete_observations_at_threshold[j,1])
+        observation_set <- read.csv(files_full_path[file_to_iterate])
         clean_observation_set <- na.omit(observation_set)
         x <- c(clean_observation_set[,2])
         y <- c(clean_observation_set[,3])
         z <- cor(x,y)
-        correlation_ccoefficients <- rbind(correlation_ccoefficients, z)
+        correlation_ccoefficients <- c(correlation_ccoefficients, z)
     }
     head(correlation_ccoefficients)
 }
