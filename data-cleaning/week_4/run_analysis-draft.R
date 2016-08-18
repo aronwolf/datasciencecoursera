c239c7e392085160b38165e7e27e09e9cbb41103
Aron Wolf <aron.wolf@proquest.com>
2016-08-18 10:19:39 -0700

    Adding sapply

--- data-cleaning/week_4/run_analysis-draft.R
+++ data-cleaning/week_4/run_analysis-draft.R
@@ -34,5 +34,7 @@
     activities <- read.table("UCI\ HAR\ Dataset/activity_labels.txt")
     mean_sd_observations$Activity <- mapvalues(mean_sd_observations$Activity, from = c(1:length(activities$V2)), to = as.vector(activities$V2))
     
+    #names(mean_sd_observations[-c(1,2)])
     #dcast(mean_sd_observations, Subject + Activity ~., mean, value.var = "fBodyBodyGyroJerkMag-std()")
+    #sapply(y, fun(x))
 ## }
